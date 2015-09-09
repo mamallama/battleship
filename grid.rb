a59a29549517509f1c5dcf542f19e5d0eaba5b87
Llamamama <reneeq25@gmail.com>
2015-09-09 15:59:26 -0400

    Place ship works on grid

--- grid.rb
+++ grid.rb
@@ -1,6 +1,7 @@
 class Grid
   def initialize
     @ships = []
+
   end
 
   def has_ship_on?(x,y)
@@ -24,15 +25,20 @@ class Grid
   end
 
   def display
-
+    y = 0
     #loop through rows, in each row, loop through columns
     puts %Q{    1   2   3   4   5   6   7   8   9   10
   -----------------------------------------}
     letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
-    (0..9).each do |row|
-      output = "#{letters[row]} |"
-      (0..9).each do |column|
-        output += "   |"
+
+    (1..10).each do |row|
+      output = "#{letters[row-1]} |"
+      (1..10).each do |column|
+        if has_ship_on?(column, row)
+          output += " O |"
+        else
+          output += "   |"
+        end
       end
       puts output
     end
